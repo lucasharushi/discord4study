8f86df18d3573f0abb84b4ba8a9dbbf09990a458
Lucas Harushi <hurashii@gmail.com>
2020-04-02 22:51:56 -0300

    organizando o handler de comandos

--- index.js
+++ index.js
@@ -102,80 +102,21 @@ bot.on('message', function(message) {
         break;
 
         case 'skip':
-            var server = servers[message.guild.id];
-            if(server.dispatcher) {
-                server.dispatcher.end();
-            }
- 
-            message.channel.send('skipping the song!');
+            bot.commands.get('skip').execute(message, args, servers);
         break;
 
         case 'stop':
-            var server = servers[message.guild.id];
-
-            if(message.guild.voiceConnection) {
-                for(var i = server.queue.length - 1; i >= 0; i--) {
-                    server.queue.splice(i, 1);
-                }
-
-                server.dispatcher.end();
-                message.channel.send('ending the queue leaving the voice channel');
-                console.log('stopped the queue');
-            }
-
-            if(message.guild.connection) {
-                message.guild.voiceConnection.disconnect();
-            }
+            bot.commands.get('stop').execute(message, args, servers);
         break;
 
         // Search images on dogpile.com
         case 'image':
-            let search = args[1];
-            if(!search) {
-                message.reply('You didnt specity search');
-                return;
-            }
-
-            image(message, search);
+            bot.commands.get('image').execute(message, args, request, cheerio);
         break;
     }
 });
 
-function image(message, search) {
-    // Get images from dogpile.com
-    var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + search,
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    }
 
-    request(options, function(error, response, responseBody) {
-        if (error) {
-            return;
-        }
-
-        $ = cheerio.load(responseBody);
-
-        var links = $('.image a.link');
-
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr('href'));
-
-        // console.log(urls);
-        if(!urls.length) {
-            return;
-        }
-
-        // Send result
-        // for(let cont = 0; cont < urls.length; cont++) {
-            // message.channel.send(urls[cont]);
-        // }
-        message.channel.send(urls[Math.floor(Math.random() * urls.length)] + " " + message.guild.members.random());
-        // this method return so many results in $urls, so generate a random value in array
-    });
-}
 
 // Bot connect
 bot.login(config.token);
